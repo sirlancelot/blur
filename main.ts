f337a0cb85c7caa3083d8bddbeb13d73e07de3a1
Matthew Pietz <sirlancelot@users.noreply.github.com>
2023-06-26 13:27:04 -0700

    Reduce branching

--- main.ts
+++ main.ts
@@ -1,89 +1,63 @@
 import { MarkdownPostProcessor, Plugin} from 'obsidian';
 
-enum ComponentChoice {
-	Default = "Default",
-}
+// Redact w/ char '█'  &block;
+const blockChar = String.fromCharCode(9608);
+const tokenConfig = {
+  '~[]': ['blur-brick', (content) => content.replace(/[^\s]/g, blockChar)],
+  '~()': ['blur-bone', (content) => content],
+  '~{}': ['blur-inline', (content) => content],
+};
 
 export default class BlurPlugin extends Plugin {
-
-  async onload() {
-		this.registerMarkdownCodeBlockProcessor("blur", this.blurBlockHandler.bind(this, null));
-		this.registerMarkdownCodeBlockProcessor("blur-brick", this.blurBlockHandler.bind(this, null));
-		this.registerMarkdownCodeBlockProcessor("blur-bone", this.blurBlockHandler.bind(this, null));
-		this.registerMarkdownPostProcessor(
-			buildPostProcessor()
-		);
-		console.log("%c Blur plugin loaded", 'color:lime;');
-	}
-
-  onunload() {
-		console.log("%c Blur plugin unloaded", 'color:lime;');
-	}
-
-  async blurBlockHandler(type: ComponentChoice, source: string, el: HTMLElement, ctx: any): Promise<any> {
-    if (el.className==='block-language-blur-brick') {
-      const block = el.createEl("div", {cls: "blur-brick-block"})
-      let inputElement: HTMLElement
-      inputElement = block.createEl("div", {text: '', cls: "blur-brick-innerblock"})
-      source.split(/\W+/).forEach((w:string) => {
-        let word = w.trim();
-        if (word !== '') {
-          //redact w/ char '█'  &block; █
-          inputElement.appendChild(createEl('code', {text: word.replace(/[^\s]/g, '█'), cls: "blur-brick" })); 
-        }
-      })
-    }
-    else if (el.className==='block-language-blur-bone') {
-      const block = el.createEl("div", {cls: "blur-bone-block"})
-      let inputElement: HTMLElement
-      inputElement = block.createEl("div", {text: '', cls: "blur-bone-innerblock"})
-      source.split(/\W+/).forEach((w:string) => {
-        let word = w.trim();
-        if (word !== '') {
-          inputElement.appendChild(createEl('code', {text: word, cls: "blur-bone"})); 
-        }
-      })
-    }
-    else if (el.className==='block-language-blur') {
-      const block = el.createEl("div", {cls: "blur-block"})
-      let inputElement: HTMLElement
-      inputElement = block.createEl("div", {text: '', cls: "blur-innerblock"})
-      source.split(/\W+/).forEach((w:string) => {
-        let word = w.trim();
-        if (word !== '') {
-          inputElement.appendChild(createEl('code', {text: word, cls: "blur-inline"})); 
-        }
-      })
-    }
+  onload() {
+		this.registerMarkdownCodeBlockProcessor("blur", blurBlockHandler);
+		this.registerMarkdownCodeBlockProcessor("blur-brick", blurBrickHandler);
+		this.registerMarkdownCodeBlockProcessor("blur-bone", blurBoneHandler);
+		this.registerMarkdownPostProcessor(buildPostProcessor());
 	}
 }
 
-export function buildPostProcessor(): MarkdownPostProcessor {
+function blurBrickHandler(source: string, el: HTMLElement, ctx: any) {
+  const block = el.createEl("div", {cls: "blur-brick-block"})
+  const inputElement: HTMLElement = block.createEl("div", {text: '', cls: "blur-brick-innerblock"})
+  const [cls, format] = tokenConfig['~[]']
+  source.split(/\W+/).forEach((word) => {
+    if (word.trim())
+      inputElement.appendChild(createEl('code', { text: format(word), cls })); 
+  })
+}
+
+function blurBoneHandler(source: string, el: HTMLElement, ctx: any) {
+  const block = el.createEl("div", {cls: "blur-bone-block"})
+  const inputElement: HTMLElement = block.createEl("div", {text: '', cls: "blur-bone-innerblock"})
+  const [cls, format] = tokenConfig['~()']
+  source.split(/\W+/).forEach((word) => {
+    if (word.trim())
+      inputElement.appendChild(createEl('code', { text: format(word), cls }));
+  })
+}
+
+function blurBlockHandler(source: string, el: HTMLElement, ctx: any) {
+  const block = el.createEl("div", {cls: "blur-block"})
+  const inputElement: HTMLElement = block.createEl("div", {text: '', cls: "blur-innerblock"})
+  const [cls, format] = tokenConfig['~{}']
+  source.split(/\W+/).forEach((word) => {
+    if (word.trim())
+      inputElement.appendChild(createEl('code', { text: format(word), cls })); 
+  })
+}
+
+function buildPostProcessor(): MarkdownPostProcessor {
 	return (el) => {
-    el.findAll("code").forEach((code) => {
-      let text = code.innerText.trim();
-      if (text.startsWith('~[') && text.endsWith(']')) {
-        let part = text.substring(1);
-        let content = part.substring(part.length-1,1);
-        code.addClass('blur-brick');
-        code.setAttribute('tabindex', '0');
-        //redact w/ char '█'  &block; █
-        code.innerText=content.replace(/[^\s]/g, '█');
-      }
-      else if (text.startsWith("~(") && text.endsWith(')')) {
-        let part = text.substring(1);
-        let content = part.substring(part.length-1,1);
-        code.addClass('blur-bone');
-        code.setAttribute('tabindex', '0');
-        code.innerText=content;
-      }
-      else if (text.startsWith('~{') && text.endsWith('}')) {
-        let part = text.substring(1);
-        let content = part.substring(part.length-1,1);
-        code.addClass('blur-inline');
-        code.setAttribute('tabindex', '0');
-        code.innerText=content;
-      }
-    })
+    for (const node of el.querySelectorAll("code")) {
+      const text = node.innerText.trim();
+      const tokens = `${text.slice(0, 2)}${text.slice(-1)}`;
+      const [cls, format] = tokenConfig[tokens] || [];
+      if (!cls) return;
+
+      node.addClass(cls);
+      node.setAttribute('tabindex', '0'); // Support `:focus` CSS
+      node.innerText = format(text.slice(2, -1));
+    }
   }
 }
